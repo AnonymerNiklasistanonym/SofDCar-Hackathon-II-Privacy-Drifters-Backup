d310a04e9ad1374ad0b2f59df164a332f649be97
irahulcse <1rahulchandra1@gmail.com>
2023-11-10 21:30:54 +0100

    Update  the changes

--- sofd-frontend/src/pages/MatchingServicePage.jsx
+++ sofd-frontend/src/pages/MatchingServicePage.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import RidingPoolMap from '../components/RidingPoolMap'
 
 const FirstPage = () => {
   return (
-    <div>FirstPage</div>
+     <RidingPoolMap/>
   )
 }
 
